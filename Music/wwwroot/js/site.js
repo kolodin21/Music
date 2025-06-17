c0396a4b976eef6d0bfd524f96d6fb720db23e02
Иван Колодин <kolodin21@yandex.ru>
2025-06-17 22:42:45 +0300

    Изменил структуру верстки файла создания альбома Добавил стили

--- Music/wwwroot/js/site.js
+++ Music/wwwroot/js/site.js
@@ -1,4 +1,14 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿"use strict";
 
-// Write your JavaScript code.
+$.fn.setLoading = function (isLoading) {
+    return this.each(function () {
+        const $btn = $(this);
+        if (isLoading) {
+            $btn.addClass('button-loading');
+            $btn.prop('disabled', true);
+        } else {
+            $btn.removeClass('button-loading');
+            $btn.prop('disabled', false);
+        }
+    });
+};
